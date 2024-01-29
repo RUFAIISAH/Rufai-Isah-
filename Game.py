6407f1ada645b13a696014d51f2dfb68d4a02655
RUFAIISAH <158082277+RUFAIISAH@users.noreply.github.com>
2024-01-29 18:31:00 +0100

    Create Game.py
    For student

---
+++ Game.py
@@ -0,0 +1,16 @@
+Tambaya = 10
+maki = 0
+suna = input("shigar da sunanka ana: ").upper()
+print("Barka da zuwa economics department." , suna)
+bugawa = input ("shin ka shiryawa wannan Text din (eh/a'a')? ").lower()
+if bugawa == "eh":
+    print ("Tohm ! ka shirya yanzu kuwa.")
+    Tambaya = input("1. menene sunan shugaban kasar Nigeria wanda ya gabata? ").lower()
+    Tambaya == "Buhari" or Tambaya == "muhammadu Buhari" or Tambaya == "GMB"
+    Tambaya = input("2. menene amsar 28 a hada da 5? ") 
+if Tambaya == "40":
+    print("dai dai ne")
+    print (" mun gode da lokacinka ")
+    print(" kaci Tambayoyi ",  "maki, dai dai" )
+    total = (maki/tambayoyi)*100
+    print = ("total ; ", total, "%") 
